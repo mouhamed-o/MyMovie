dcee7ce5e358d9c1488e89c78e2c5ba53b65d10d
mouhamed-o <mohamed-oumarou@live.com>
2019-10-02 11:00:42 +0200

    init project structure

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MoviesService} from '../../movies.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss'],
+  providers: [MoviesService]
+})
+export class CategoryComponent implements OnInit {
+  genres: Array<Object>;
+  
+  constructor(private _moviesServices: MoviesService) {
+    this._moviesServices.getGenres().subscribe(res => {
+      this.genres = res.genres.slice(0, 20);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import {MoviesService} from '../../movies.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  popularList: Array<Object>;
+  theatersList: Array<Object>;
+  topRatedList: Array<Object>;
+  searchRes: Array<Object>;
+  searchStr: string;
+
+  constructor(private _moviesService: MoviesService) {
+    this._moviesService.getPopular().subscribe(res => {
+      this.popularList = res.results;
+    });
+    this._moviesService.getInTheaters().subscribe(res => {
+      this.theatersList = res.results;
+    });
+    this._moviesService.getTopRatedMovies().subscribe(res => {
+      this.topRatedList = res.results;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  searchMovies() {
+    this._moviesService.searchMovies(this.searchStr).subscribe(res => {
+      this.searchRes = res.results;
+    })
+  }
+
+}
